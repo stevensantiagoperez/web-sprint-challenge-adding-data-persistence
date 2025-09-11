c4320537a6812e0f6d9c21c84f061aaecd8595b2
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-09-10 23:51:50 -0400

    build server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // build your server here and require it from index.js
+
+const express = require('express')
+const projectsRouter = require('./task/router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/project', projectsRouter)
+
+module.exports = server

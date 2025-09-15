b73f83ae4c17e90d4b57ff8f0f5e3dfb663dfb33
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-09-14 20:47:41 -0400

    server working

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 // build your server here and require it from index.js
 
 const express = require('express')
-const projectsRouter = require('./task/router')
+const projectRouter = require('./project/router')
+const taskRouter = require('./task/router')
 
 const server = express()
 
 server.use(express.json())
 
-server.use('./api/project', projectsRouter)
+server.use('/api/project', projectRouter)
+server.use('/api/task', taskRouter)
 
 module.exports = server
--- api/task/router.js
+++ api/task/router.js
@@ -1,13 +1,13 @@
 // build your `/api/tasks` router here
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
+router.get('/',  (req, res) => {
   res.json({ api: "up" });
 });
 
 router.use((err, req, res, next) => { // eslint-disable-line
   res.status(500).json({
-    customMessage: "something went wrong in the project router",
+    customMessage: "something went wrong in the task router",
     message: err.message,
     stack: err.stack,
   });

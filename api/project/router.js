30405408d11aa38041ccfb7ddd753f8549219ffe
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-09-14 20:57:56 -0400

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,5 +1,6 @@
 // build your `/api/projects` router here
 const router = require("express").Router();
+const Project = require('./model')
 
 router.get("/", (req, res) => {
   res.json({ api: "up" });
--- api/task/router.js
+++ api/task/router.js
@@ -1,5 +1,6 @@
 // build your `/api/tasks` router here
 const router = require("express").Router();
+const Task = require('./model');
 
 router.get('/',  (req, res) => {
   res.json({ api: "up" });
